5a8180853ffdf2325e38adcbfd1fc3669d7a7204
Falconii <falconi.2050@gmail.com>
2023-11-07 16:54:34 -0300

    Atualização B

--- util/empresaParametros.js
+++ util/empresaParametros.js
@@ -1,8 +1,10 @@
 const erroDB = require('../util/userfunctiondb');
 
-exports.empresas = function() { 
-const parametros = { 
-	};
-	return parametros; 
-} 
-
+exports.empresas = function() {
+    const parametros = {
+        razao: { check: true, require: true, maxLength: 40 },
+        fantasi: { check: true, require: false, maxLength: 40 },
+        inscri: { check: true, require: false, maxLength: 14 },
+    };
+    return parametros;
+}
